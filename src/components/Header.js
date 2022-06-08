918562e48118d3ec3664e2283c14cee89d3bb14a
Josh Shearer <ShearerJosh@hotmail.com>
2022-06-08 10:25:41 -0700

    href update

--- src/components/Header.js
+++ src/components/Header.js
@@ -59,7 +59,7 @@ function Header(props) {
                         <Menu.Item>
                           {({ active }) => (
                             <a
-                              href="joshshearer.org"
+                              href="https://www.joshshearer.org"
                               className={classNames(
                                 active ? "bg-gray-100" : "",
                                 "block px-4 py-2 text-sm text-gray-700"
